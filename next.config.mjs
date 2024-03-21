c3e4b1530e6b11bbf7acc3951e76cf05ca8a9160
Hitesh01839 <gangahitesh04@gmail.com>
2024-03-22 01:53:36 +0530

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*", // Match all API routes
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Allow requests from all origins (not recommended for production)
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
